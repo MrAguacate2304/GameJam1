c9be05531bc6abda5e7db502a0684f78f5a2ede4
MrAguacate23 <alan.castillo.e@gmail.com>
2022-04-05 09:34:58 +0200

    contador
    Contador es queda parat a 0

---
+++ Assets/ScriptRamon/Timer_Controlle.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer_Controlle : MonoBehaviour
+{
+    public Text contador;
+    private float time = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contador.text = " " + time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time >= 0 )
+        {
+            time -= Time.deltaTime;
+            contador.text = " " + time.ToString("f0");
+        }
+        else if (time < 1)
+        {
+            time = 0;
+            contador.text = " " + time.ToString("f0");
+        }
+    }
+}
