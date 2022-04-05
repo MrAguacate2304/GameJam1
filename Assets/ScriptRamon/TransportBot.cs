a597a061cf34c57b86fdf015b3d31fffebe63df5
rvallbona <joan.vallbona@enti.cat>
2022-04-05 12:26:24 +0200

    Actualizacion

--- Assets/ScriptRamon/TransportBot.cs
+++ Assets/ScriptRamon/TransportBot.cs
@@ -10,25 +10,36 @@ public class TransportBot : MonoBehaviour
     int lastPos = -1;
     GameObject currentDestination;
     public GameObject endPrefab;
+    float contador;
+    public float contador1;
+    public Transform[] posicionBot;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        maxVectors = 3;
+        maxVectors = 2;
         posBot = new Vector2[maxVectors];
         posBot[0] = new Vector2(7.35f, 2.44f);
         posBot[1] = new Vector2(-6.6f, 2.74f);
+        contador = 0;
         transportBot();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        contador += Time.deltaTime;
+        if (contador >= contador1)
+        {
+            transportBot();
+            contador = 0;
+        }
     }
 
     void transportBot() 
     {
+        maxVectors = posicionBot.Length;
         numrandom = Random.Range(0, maxVectors);
             if (numrandom == lastPos && numrandom < maxVectors / 2)
             {
@@ -38,6 +49,6 @@ public class TransportBot : MonoBehaviour
             {
                 numrandom -= 1;
             }
-            transform.position = posBot[numrandom];      
+            transform.position = posicionBot[numrandom].position;      
     }
 }
